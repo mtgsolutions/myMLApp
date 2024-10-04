ae2f33a9b2e0358023a889a9a718a7924830dd64
Milton Santos <miltont.santos@outlook.com>
2024-10-04 00:14:05 -0300

    Add project files.

---
+++ myMLApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyMLApp;
+
+namespace myMLApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read avaliation 
+            Console.WriteLine("Enter your sentiment after eat our food: ");
+            var avaliation = Console.ReadLine();
+            //Load sample data
+            var sampleData = new Sentiment.ModelInput()
+            {
+                Col0 = @avaliation,
+            };
+
+            //Load model and predict output
+            var result = Sentiment.Predict(sampleData);
+
+            // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+            var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+        }
+    }
+}
